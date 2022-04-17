7a70625c014f94aac11c3c8b710ffe3e49f7f454
10994015 <a0938599191@gmail.com>
2022-04-17 21:14:01 +0800

    0417

--- cms/userData.php
+++ cms/userData.php
@@ -25,28 +25,84 @@ if(isset($_SESSION['username'])){
             margin:0;
             padding: 0;
         }
-    .list{
-        width:100%;
-        padding: 10px 20px;
-        border-bottom: 1px #666 solid;
-    }
-    .list > p{
-        margin:5px 0;
-    }
+        body{
+            padding: 20px;
+        }
+        .list{
+            width:100%;
+            padding: 10px 20px;
+            border-bottom: 1px #666 solid;
+        }
+        .list > p{
+            margin:5px 0;
+        }
+        
+        table, th , td{
+            border:1px #000 solid;
+            border-collapse: collapse;
+            padding: 10px;
+        }
+        table th{
+            background-color: #aaa;
+        }
+        table th:nth-child(1){
+            width:50px;
+        }
+        table th:nth-child(2){
+            width:250px;
+        }
+        table th:nth-child(1){
+            width:100px;
+        }
+        table th:nth-child(1){
+            width:100px;
+        }
+        table th:nth-child(1){
+            width:50px;
+        }
+        table td{
+            padding: 10px;
+        }
+        #btn{
+            margin-bottom: 10px;
+        }
     </style>
 </head>
 <body>
-    <div id="app">
-    <?php foreach($RS_user as $item){ ?>
-        <div class="list">
-            <p><b>姓名:</b><?php echo $item['student'];?></p>
-            <p><b>移動座標:</b><?php echo $item['coor'];?></p>
-            <p><b>開始作答時間:</b><?php echo $item['timeStart'];?></p>
-            <p><b>結束作答時間:</b><?php echo $item['timeEnd'];?></p>
-            <p><b>是否答對:</b><?php echo $item['bingo'];?></p>
-        </div>
-    <?php }?>
-    </div>
+    <button id="btn">匯出Excel</button>
+    <table id="table">
+        <thead>
+            <tr>
+                <th>姓名</th>
+                <th>移動座標</th>
+                <th>開始時間</th>
+                <th>結束時間</th>
+                <th>是否答對</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($RS_user as $item){ ?>
+            <tr>
+                <td><?php echo $item['student']; ?></td>
+                <td><?php echo $item['coor']; ?></td>
+                <td><?php echo $item['timeStart']; ?></td>
+                <td><?php echo $item['timeEnd']; ?></td>
+                <td><?php echo $item['bingo']; ?></td>
+            </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+
+    <script src="../js/table2excel.js"></script>
+    <script>
+    const btn = document.getElementById('btn');
+   
+    btn.addEventListener('click',()=>{
+        var table2excel = new Table2Excel();
+        table2excel.export(document.querySelectorAll("table"));
+    })
+    
+    </script>
 </body>
 </html>
 
